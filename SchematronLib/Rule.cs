65bf359ad85466072ab2870b3247bc47ec90e597
lundst1 <rickard.lundstedt@gmail.com>
2024-07-17 13:11:05 +0200

    Add project files.

---
+++ SchematronLib/Rule.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchematronLib
+{
+    public class Rule
+    {
+        //Private variable 
+        private string context;
+        private string definition;
+        private string message;
+        private bool assert;
+        private bool report;
+
+        private Rule(string context, string definition, string message, bool assert, bool report)
+        {
+            this.context = context;
+            this.definition = definition;
+            this.message = message;
+            this.assert = assert;
+            this.report = report;
+        }
+
+    }
+}
---
+++ SchematronLib/XMLFile.cs
@@ -0,0 +1,50 @@
+﻿using System.Linq;
+using System.Xml.Linq;
+
+namespace SchematronLib
+{
+    public class XMLFile
+    {
+        //Private string variable for the file name.
+        private string filename;
+        //Private string variable for the namespace
+        private string nameSpace;
+        //Private variable for loading all elements into memory.
+        private XElement elements;
+
+        public string Filename
+        {
+            get { return filename; }
+            set { filename = value; }
+        }
+        public string NameSpace
+        {
+            get { return nameSpace; }
+        }
+        public XElement Elements
+        {
+            get { return elements; }
+        }
+
+        public XMLFile(string filename)
+        {
+            this.filename = filename;
+            ReadFile();
+
+        }
+        private void ReadFile()
+        {
+            elements = XElement.Load(filename);
+        }
+        private void SetNameSpace()
+        {
+            nameSpace = elements.GetDefaultNamespace().NamespaceName;
+        }
+        //Todo: Antingen returnerar du listan så får Processor iterera över den
+        // Eller så publicerar du en händelse som processor lyssnar på och jämför regler med. 
+        public void Traverse()
+        {
+
+        }
+    }
+}
